f6ab96a2ffa907fb1a3336b65ee36bf4dc2bb883
Anastasiia <na.konsta@gmail.com>
2024-08-29 15:29:29 +0300

    fix: add models

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from app.handlers import router
+from app.database.models import async_main
 
 async def main():
+  await async_main()
   bot = Bot(token='7174111304:AAGFualxjpWBVPlpn5Vh9zuRx8afY_XOKR8')
   dp = Dispatcher()
   dp.include_router(router)
