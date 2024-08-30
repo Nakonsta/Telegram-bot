e785c91dba6135b62c8a7d331ab3c4cd27ab36d0
Anastasiia <na.konsta@gmail.com>
2024-08-30 14:05:58 +0300

    fix: add catalog request and handler

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,1 +1,12 @@
- 
+from app.database.models import async_session
+from app.database.models import User, Category, Item
+from sqlalchemy import select, update, delete, desc
+
+async def set_user(tg_id):
+  async with async_session() as session:
+    user = await session.scalar(select(User).where(User.id == tg_id))
+    await session.commit()
+
+async def get_categories():
+  async with async_session() as session:
+    return await session.scalars(select(Category))
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,13 +1,24 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, 
   InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from app.database.requests import get_categories 
 
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],[KeyboardButton(text='Корзина')], [KeyboardButton(text='Контакты'), KeyboardButton(text='О нас')]],
   resize_keyboard=True,input_field_placeholder='Выберите пункт меню')
 
-catalog = InlineKeyboardMarkup(inline_keyboard=[
-  [InlineKeyboardButton(text='Футболки', callback_data='t-shirt')],
-  [InlineKeyboardButton(text='Кроссовки', callback_data='sneakers')], 
-  [InlineKeyboardButton(text='Кепки', callback_data='cap')]
-])
+# catalog = InlineKeyboardMarkup(inline_keyboard=[
+#   [InlineKeyboardButton(text='Футболки', callback_data='t-shirt')],
+#   [InlineKeyboardButton(text='Кроссовки', callback_data='sneakers')], 
+#   [InlineKeyboardButton(text='Кепки', callback_data='cap')]
+# ])
+
+async def categories():
+  all_categories = await get_categories()
+  keyboard = InlineKeyboardBuilder()
+  for category in all_categories:
+    keyboard.add(InlineKeyboardButton(text=category.name, callback_data=f"category_{category.id}"))
+  keyboard.add(InlineKeyboardButton(text='На главную', callback_data='to_main'))
+  return keyboard.adjust(2).as_markup()
+
 
 get_number = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Отправить номер', request_contact=True)]], resize_keyboard=True)
